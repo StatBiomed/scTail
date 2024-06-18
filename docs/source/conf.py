b8427f39d2f8c6f93c6706d6abd7e09d1a96dd2a
houruiyan <ruiyan_hou@163.com>
2024-06-18 16:34:38 +0800

    add

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -76,8 +76,7 @@ if os.environ.get('READTHEDOCS') == 'True':
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-import sphinx_autodoc_typehints
-import nbsphinx
+
 
 needs_sphinx = "1.7"
 
